e303f5326e5b08a21d803ebca734ae98ff8a4c03
sar245 <sar_584@yahoo.co.in>
2017-05-07 21:43:51 +0530

    Dao changes

---
+++ BadmintonAnalyticsWS/src/main/java/com/analytics/badmintion/beans/Player.java
@@ -0,0 +1,173 @@
+package com.analytics.badmintion.beans;
+
+import java.util.Date;
+import javax.persistence.*;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table( name = "Player" )
+public class Player {
+	@Id
+	@GeneratedValue(generator="increment")
+	@Column(name = "playerId")	
+	private int playerId;
+	@Column(name = "firstName")
+	private String firstName;
+	@Column(name = "lastName")
+	private String lastName;
+	@Column(name = "middleName")
+	private String middleName;
+	@Column(name = "country")
+	private String country;
+	@Column(name = "gender")
+	private String gender;
+	@Column(name = "dateOfBirth")
+	private Date dateOfBirth;
+	@Column(name = "placeOfBirth")
+	private String placeOfBirth;
+	@Column(name = "primaryHand")
+	private String primaryHand;
+
+	public Player(int playerId, String firstName, String lastName,
+			String middleName, String country, String gender, Date dateOfBirth,
+			String placeOfBirth, String primaryHand) {
+		super();
+		this.playerId = playerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.middleName = middleName;
+		this.country = country;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.placeOfBirth = placeOfBirth;
+		this.primaryHand = primaryHand;
+	}
+
+	/**
+	 * @return the playerId
+	 */
+	public int getPlayerId() {
+		return playerId;
+	}
+
+	/**
+	 * @param playerId the playerId to set
+	 */
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the middleName
+	 */
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	/**
+	 * @param middleName the middleName to set
+	 */
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the dateOfBirth
+	 */
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	/**
+	 * @return the placeOfBirth
+	 */
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
+
+	/**
+	 * @param placeOfBirth the placeOfBirth to set
+	 */
+	public void setPlaceOfBirth(String placeOfBirth) {
+		this.placeOfBirth = placeOfBirth;
+	}
+
+	/**
+	 * @return the primaryHand
+	 */
+	public String getPrimaryHand() {
+		return primaryHand;
+	}
+
+	/**
+	 * @param primaryHand the primaryHand to set
+	 */
+	public void setPrimaryHand(String primaryHand) {
+		this.primaryHand = primaryHand;
+	}
+
+}
---
+++ BadmintonAnalyticsWS/src/main/java/com/analytics/badmintion/controller/BAWSPlayerController.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.analytics.badmintion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+//import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author asubramanian
+ *
+ */
+@Controller
+@RequestMapping("/baws/player")
+public class BAWSPlayerController {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	/**
+	 * @return the sessionFactory
+	 */
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	/**
+	 * @param sessionFactory
+	 *            the sessionFactory to set
+	 */
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	public @ResponseBody int getPlayer(@PathVariable("id") int id) {
+
+		return 0;
+	}
+}
