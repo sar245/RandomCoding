3007f6bb3fe3165cdc967882927cecd92477a0de
sar245 <sar_584@yahoo.co.in>
2017-05-07 12:02:34 +0530

    Sample WS is up

--- BadmintonAnalyticsWS/src/main/java/com/analytics/badmintion/BAWSConfiguration.java
+++ BadmintonAnalyticsWS/src/main/java/com/analytics/badmintion/BAWSConfiguration.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.analytics.badmintion.configuration;
+package com.analytics.badmintion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
