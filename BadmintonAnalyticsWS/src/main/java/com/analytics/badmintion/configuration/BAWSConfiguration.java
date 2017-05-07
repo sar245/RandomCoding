ef6b76deeb847d484402b35d9b36e1ab6d235325
sar245 <sar_584@yahoo.co.in>
2017-05-07 10:54:54 +0530

    Badminton Analytics First upload

---
+++ BadmintonAnalyticsWS/src/main/java/com/analytics/badmintion/configuration/BAWSConfiguration.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.analytics.badmintion.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sars
+ *
+ */
+@SpringBootApplication
+public class BAWSConfiguration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(BAWSConfiguration.class, args);
+	}
+
+}
---
+++ BadmintonAnalyticsWS/src/main/java/com/analytics/badmintion/controller/BAWSController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.analytics.badmintion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author sars
+ *
+ */
+@Controller
+@RequestMapping("/baws/player")
+public class BAWSController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody int sayHello(
+			@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
+	if ("mark".equalsIgnoreCase(name)) {
+		return 1;
+	}
+		return 0;
+	}
+}
